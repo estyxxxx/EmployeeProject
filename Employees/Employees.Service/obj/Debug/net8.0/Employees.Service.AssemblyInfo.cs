6798bf2bc4ed24601cefeca33a9c302d36f5e599
estyxxxx <st0548457692@gmail.com>
2024-04-15 03:14:48 +0300

    Eighth

--- Employees/Employees.Service/obj/Debug/net8.0/Employees.Service.AssemblyInfo.cs
+++ Employees/Employees.Service/obj/Debug/net8.0/Employees.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employees.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0822da9da218691fe2c25082edc2024e6e0c97e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8130659f5860459ab759d7567e187f4709edf875")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employees.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employees.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
